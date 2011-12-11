52145b23f9c0d476f9f178359003386997aedcb4
Tom Greuter <tomgreuter@xs4all.nl>
2011-12-11 22:25:51 +0100

    Added Fronteers Jasmine example code and tests

---
+++ src/Fronteers.js
@@ -0,0 +1,22 @@
+function FronteersShop() {
+}
+FronteersShop.prototype.calcDiscount = function(price, customerType) {
+
+	// Check if parameter price is correct, must be floating point number
+	if (isNaN(parseFloat(price)) || (!isFinite(price)) ) {
+		return;
+	}
+	// Check if parameter customerType is correct of type String
+	if (typeof customerType !== 'string') {
+		return;
+	}
+
+	if (customerType === 'member') {
+		return price * 0.8;
+	} else if (customerType === 'fronteers') {
+		return price * 0.5;
+	} else {
+		return price;
+	}
+};
+
