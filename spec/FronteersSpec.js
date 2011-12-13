f4f870deb86d760af5ceaee1dd485482dee7d7e8
Tom Greuter <tomgreuter@xs4all.nl>
2011-12-13 22:08:59 +0100

    Commented out all spec and source code, so Jasmine tests 0 specs

--- spec/FronteersSpec.js
+++ spec/FronteersSpec.js
@@ -12,27 +12,27 @@ describe("FronteersShop", function() {
 
   describe("when the discount price is calculated", function() {
 
-    it("should correctly validate function input", function() {
-      // Wrong number of expected arguments
-      expect(fs.calcDiscount(1)).toBeUndefined();
-      // First argument of wrong data type
-      expect(fs.calcDiscount(null, CLIENTTYPE_FRONTEERS)).toBeUndefined();
-      // Second argument of wrong data type
-      expect(fs.calcDiscount(100, null)).toBeUndefined();
-      // Input should be accepted
-      expect(fs.calcDiscount(100, CLIENTTYPE_FRONTEERS)).toBeDefined();
-    });
+    // it("should correctly validate function input", function() {
+    //   // Wrong number of expected arguments
+    //   expect(fs.calcDiscount(1)).toBeUndefined();
+    //   // First argument of wrong data type
+    //   expect(fs.calcDiscount(null, CLIENTTYPE_FRONTEERS)).toBeUndefined();
+    //   // Second argument of wrong data type
+    //   expect(fs.calcDiscount(100, null)).toBeUndefined();
+    //   // Input should be accepted
+    //   expect(fs.calcDiscount(100, CLIENTTYPE_FRONTEERS)).toBeDefined();
+    // });
 
-    it("should correctly calculate discount", function() {
-      // Expect 50% of 100 => 50
-      expect(fs.calcDiscount(100, CLIENTTYPE_FRONTEERS)).toEqual(50);
-      // Expect 80% of 100 => 80
-      expect(fs.calcDiscount(100, CLIENTTYPE_MEMBER)).toEqual(80);
-      // Expect 100% of 100 => 100
-      expect(fs.calcDiscount(100, CLIENTTYPE_NONMEMBER)).toEqual(100);
-      // Check decimals, expect 50% of 17.1 => 8.55
-      expect(fs.calcDiscount(17.1, CLIENTTYPE_FRONTEERS)).toEqual(8.55);
-    });
+    // it("should correctly calculate discount", function() {
+    //   // Expect 50% of 100 => 50
+    //   expect(fs.calcDiscount(100, CLIENTTYPE_FRONTEERS)).toEqual(50);
+    //   // Expect 80% of 100 => 80
+    //   expect(fs.calcDiscount(100, CLIENTTYPE_MEMBER)).toEqual(80);
+    //   // Expect 100% of 100 => 100
+    //   expect(fs.calcDiscount(100, CLIENTTYPE_NONMEMBER)).toEqual(100);
+    //   // Check decimals, expect 50% of 17.1 => 8.55
+    //   expect(fs.calcDiscount(17.1, CLIENTTYPE_FRONTEERS)).toEqual(8.55);
+    // });
 
   });
 
--- src/Fronteers.js
+++ src/Fronteers.js
@@ -2,21 +2,21 @@ function FronteersShop() {
 }
 FronteersShop.prototype.calcDiscount = function(price, customerType) {
 
-	// Check if parameter price is correct, must be floating point number
-	if (isNaN(parseFloat(price)) || (!isFinite(price)) ) {
-		return;
-	}
-	// Check if parameter customerType is correct of type String
-	if (typeof customerType !== 'string') {
-		return;
-	}
+	// // Check if parameter price is correct, must be floating point number
+	// if (isNaN(parseFloat(price)) || (!isFinite(price)) ) {
+	// 	return;
+	// }
+	// // Check if parameter customerType is correct of type String
+	// if (typeof customerType !== 'string') {
+	// 	return;
+	// }
 
-	if (customerType === 'member') {
-		return price * 0.8;
-	} else if (customerType === 'fronteers') {
-		return price * 0.5;
-	} else {
-		return price;
-	}
+	// if (customerType === 'member') {
+	// 	return price * 0.8;
+	// } else if (customerType === 'fronteers') {
+	// 	return price * 0.5;
+	// } else {
+	// 	return price;
+	// }
 };
 
